dfd60b8023b294a3891b210098aef2dfca7786cb
Josh LaFayette <joshlafayette@gmail.com>
2022-05-22 14:19:43 -0700

    Fix 1 and 2

--- exercises/002_std.zig
+++ exercises/002_std.zig
@@ -10,11 +10,11 @@
 //
 // Please complete the import below:
 //
-
-??? = @import("std");
+const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() void {
-    std.debug.print("Standard Library.\n", .{});
+    print("Standard Library.\n", .{});
 }
 
 // For the curious: Imports must be declared as constants because they
